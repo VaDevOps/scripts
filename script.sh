2b2190409d67c48d3afbf457a9f0d6fb6a5f3fae
VaDevOps <109411188+VaDevOps@users.noreply.github.com>
2022-07-16 11:05:40 +0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+cd ~/pathfinder
+git fetch
+git checkout v0.2.5-alpha
+cargo build --release --bin pathfinder
+mv ~/pathfinder/target/release/pathfinder /usr/local/bin/
+cd py
+source .venv/bin/activate
+PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt
+systemctl restart starknetd
+pathfinder -V
